b71a87bc6cb18ce4a4e6185cce121631a55a7157
= <=>
2026-05-05 15:01:25 +0800

    Updated index.php to trigger pipeline

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // Sample PHP file
-$name = "Mr CNAS";
-$message = "Hello, $name! Welcome to  CNAS 1.";
+$name = "Mr  Dylan- CNAS";
+$message = "Hello, $name! CNAS has been changed.";
 echo $message;
 ?>
+
